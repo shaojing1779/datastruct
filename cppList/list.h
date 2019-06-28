14d19ebe1825c44fdde41248dfe023b84d59745c
shaojing <jing.shao@wetax.com.cn>
2019-06-28 11:21:10 +0800

    Dlist

--- cppList/list.h
+++ cppList/list.h
@@ -81,4 +81,85 @@ private:
     pthread_mutex_t _mlock = PTHREAD_MUTEX_INITIALIZER;
 };
 
+template<typename T>
+class DList
+{
+public:
+    DList()
+    {
+        _head = new Node();
+        _tail = new Node();
+        _head->_prev = nullptr;
+        _head->_next = _tail;
+        _tail->_prev = _head;
+        _tail->_next = nullptr;
+    }
+    void push_back(const T& t)
+    {
+        pthread_mutex_lock(&_mlock);
+
+        Node* n = new Node(t);
+
+        n->_t = t;
+        n->_prev = _head;
+		n->_next = _head->_next;
+
+		_head->_next->_prev = n;
+		_head->_next = n;
+
+		_size++;
+
+        pthread_mutex_unlock(&_mlock);
+    }
+    size_t size()
+    {
+        return _size;
+    }
+
+    T pop()
+    {
+		pthread_mutex_lock(&_mlock);
+		T t_tmp;
+		do  
+		{   
+			if(!_tail->_prev->_prev) break;
+			Node* p = _tail->_prev->_prev;
+			t_tmp = _tail->_prev->_t;
+			delete _tail->_prev;
+
+			p->_next = _tail;
+			_tail->_prev = p;
+			_size--;
+
+		} while(0);
+		pthread_mutex_unlock(&_mlock);
+		return t_tmp;
+    }
+
+private:
+    class Node{
+
+    public:
+        Node()
+        {
+            _next = nullptr;
+            _prev = nullptr;
+        }
+        Node(const T& t)
+        {
+            _t = t;
+            _prev = nullptr;
+            _next = nullptr;
+        }
+        T _t;
+        Node* _next;
+        Node* _prev;
+    };
+
+    Node* _head = nullptr;
+    Node* _tail = nullptr;
+    size_t _size = 0;
+    pthread_mutex_t _mlock = PTHREAD_MUTEX_INITIALIZER;
+};
+
 #endif // LIST_H
